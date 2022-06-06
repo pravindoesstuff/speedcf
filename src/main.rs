191d33bdd3826d97bfc02f4f4c5dd6a5b75744d3
Pravin Ramana <pravinramana25@protonmail.ch>
2022-06-05 20:40:28 -0500

    Improve loading indicator

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod net;
 
 #[tokio::main]
 async fn main() {
-
+    net::test_download(1000000000).await.unwrap();
 }
--- src/net.rs
+++ src/net.rs
@@ -6,17 +6,23 @@ lazy_static::lazy_static! {
     static ref CLIENT: reqwest::Client = reqwest::Client::new();
 }
 
-pub async fn test_download(target_size: usize) -> Result<(), reqwest::Error> {
+pub async fn test_download(target_size: u64) -> Result<(), reqwest::Error> {
     let mut stream = CLIENT
         .get(format!("https://speed.cloudflare.com/__down?bytes={}", target_size))
         .send()
         .await?
         .bytes_stream();
-    let mut downloaded_size: usize = 0;
+    let pb = indicatif::ProgressBar::new(target_size);
+
+    pb.set_style(indicatif::ProgressStyle::default_bar()
+                 .template("[{elapsed_precise:.cyan/blue}] [{bar:40}] {bytes_per_sec} {spinner}")
+                 .progress_chars("=> "));
 
     while let Some(block) = stream.next().await {
-        let chunk = block?;
-        downloaded_size += chunk.len();
+        pb.inc(block?.len() as u64)
     }
+
+    pb.finish_with_message("Finished Downloado");
+    pb.finish_and_clear();
     Ok(())
 }
